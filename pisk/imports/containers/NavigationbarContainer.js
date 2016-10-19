ecc4495636b0f86ab20e39b95d04a4fd629bac7c
Uy Tran <uqtontran@gmail.com>
2016-10-19 09:57:35 +0200

    remove meteor

--- pisk/imports/containers/NavigationbarContainer.js
+++
@@ -1,9 +0,0 @@
-import {composeWithTracker} from 'react-komposer';
-import {Meteor} from 'meteor/meteor';
-
-const NavigationbarComposer = (context, onData) => {
-
-
-};
-
-export default composeWithTracker(NavigationbarComposer)(Navigationbar);
